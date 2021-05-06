575bdf9ea0f37e871b01d55d1a26fcbfe53d4f92
SystemGlitch <jeremy.la@outlook.fr>
2021-05-06 16:04:31 +0200

    Handle more validation rules

--- validation.go
+++ validation.go
@@ -2,6 +2,8 @@ package openapi3
 
 import (
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"goyave.dev/goyave/v3/validation"
@@ -12,12 +14,19 @@ func ConvertToBody(rules *validation.Rules) *openapi3.RequestBodyRef {
 	if rules == nil {
 		return nil
 	}
+
+	encodings := map[string]*openapi3.Encoding{}
+
 	// TODO cache using a simple map[*validation.Rules]*openapi3.RequestBodyRef
 	schema := openapi3.NewObjectSchema()
 	for name, field := range rules.Fields {
-		schema.Properties[name] = &openapi3.SchemaRef{Value: SchemaFromField(field)}
+		s, encoding := SchemaFromField(field)
+		schema.Properties[name] = &openapi3.SchemaRef{Value: s}
 		if field.IsRequired() {
 			schema.Required = append(schema.Required, name)
+		}
+		if encoding != nil {
+			encodings[name] = encoding
 		}
 	}
 
@@ -33,6 +42,9 @@ func ConvertToBody(rules *validation.Rules) *openapi3.RequestBodyRef {
 				}
 			}
 			content["application/json"] = openapi3.NewMediaType().WithSchema(jsonSchema)
+		}
+		if len(encodings) != 0 {
+			content["multipart/form-data"].Encoding = encodings
 		}
 	} else {
 		content = openapi3.NewContentWithJSONSchema(schema)
@@ -55,7 +67,8 @@ func ConvertToQuery(rules *validation.Rules) []*openapi3.ParameterRef {
 	parameters := make([]*openapi3.ParameterRef, 0, len(rules.Fields))
 	for name, field := range rules.Fields {
 		param := openapi3.NewQueryParameter(name)
-		param.Schema = &openapi3.SchemaRef{Value: SchemaFromField(field)}
+		s, _ := SchemaFromField(field)
+		param.Schema = &openapi3.SchemaRef{Value: s}
 		format := param.Schema.Value.Format
 		if format != "binary" && format != "bytes" {
 			param.Required = field.IsRequired()
@@ -67,7 +80,7 @@ func ConvertToQuery(rules *validation.Rules) []*openapi3.ParameterRef {
 }
 
 // SchemaFromField convert a validation.Field to OpenAPI Schema.
-func SchemaFromField(field *validation.Field) *openapi3.Schema {
+func SchemaFromField(field *validation.Field) (*openapi3.Schema, *openapi3.Encoding) {
 	// TODO save schema ref to refs
 	s := openapi3.NewSchema()
 	if rule := findFirstTypeRule(field); rule != nil {
@@ -77,7 +90,6 @@ func SchemaFromField(field *validation.Field) *openapi3.Schema {
 		case "bool":
 			s.Type = "boolean"
 		case "file":
-			// TODO format "binary" (or "bytes" ?)
 			s.Type = "string"
 			s.Format = "binary"
 		case "array": // TODO multidimensional arrays
@@ -85,18 +97,23 @@ func SchemaFromField(field *validation.Field) *openapi3.Schema {
 			schema := openapi3.NewSchema()
 			schema.Type = ruleNameToType(rule.Name)
 			s.Items = &openapi3.SchemaRef{Value: schema}
-		// TODO objects and string formats
-		// TODO email, uuid, uri, ipv4, ipv6, date, date-time (not types but patterns)
+		// TODO objects
 		default:
 			s.Type = rule.Name
 		}
 	}
 
+	var encoding *openapi3.Encoding
 	for _, r := range field.Rules {
-		ruleConverters[r.Name](r, s)
+		if (r.Name == "image" || r.Name == "mime") && encoding == nil {
+			encoding = openapi3.NewEncoding()
+		}
+		if converter, ok := ruleConverters[r.Name]; ok {
+			converter(r, s, encoding)
+		}
 	}
 	s.Nullable = field.IsNullable()
-	return s
+	return s, encoding
 }
 
 // HasFile returns true if the given set of rules contains at least
@@ -157,14 +174,16 @@ func ruleNameToType(name string) string {
 	default:
 		return name
 	}
-	// TODO match type rules with correct openapi types defined in spec
 }
 
-type RuleConverter func(r *validation.Rule, s *openapi3.Schema)
+// RuleConverter sets a schema's fields to values matching the given validation
+// rule, if supported.
+type RuleConverter func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding)
 
 var (
+	// TODO object
 	ruleConverters = map[string]RuleConverter{
-		"min": func(r *validation.Rule, s *openapi3.Schema) {
+		"min": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
 			switch s.Type {
 			case "string":
 				min, _ := strconv.ParseUint(r.Params[0], 10, 64)
@@ -177,7 +196,7 @@ var (
 				s.MinItems = min
 			}
 		},
-		"max": func(r *validation.Rule, s *openapi3.Schema) {
+		"max": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
 			switch s.Type {
 			case "string":
 				max, _ := strconv.ParseUint(r.Params[0], 10, 64)
@@ -190,7 +209,7 @@ var (
 				s.MaxItems = &max
 			}
 		},
-		"between": func(r *validation.Rule, s *openapi3.Schema) {
+		"between": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
 			switch s.Type {
 			case "string":
 				min, _ := strconv.ParseUint(r.Params[0], 10, 64)
@@ -209,7 +228,7 @@ var (
 				s.MaxItems = &max
 			}
 		},
-		"size": func(r *validation.Rule, s *openapi3.Schema) {
+		"size": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
 			switch s.Type {
 			case "string":
 				length, _ := strconv.ParseUint(r.Params[0], 10, 64)
@@ -225,7 +244,109 @@ var (
 				s.MaxItems = &count
 			}
 		},
+		"distinct": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.UniqueItems = true
+		},
+		"digits": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Pattern = "^[0-9]*$"
+		},
+		"regex": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Pattern = r.Params[0]
+		},
+		"email": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Pattern = "^[^@\\r\\n\\t]{1,64}@[^\\s]+$"
+		},
+		"alpha": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Pattern = "^[\\pL\\pM]+$"
+		},
+		"alpha_dash": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Pattern = "^[\\pL\\pM0-9_-]+$"
+		},
+		"alpha_num": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Pattern = "^[\\pL\\pM0-9]+$"
+		},
+		"starts_with": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Pattern = "^" + r.Params[0]
+		},
+		"ends_with": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Pattern = r.Params[0] + "$"
+		},
+		"ipv4": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Format = "ipv4"
+		},
+		"ipv6": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Format = "ipv6"
+		},
+		"url": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Format = "uri"
+		},
+		"uuid": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Format = "uuid"
+		},
+		"mime": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			encoding.ContentType = strings.Join(r.Params, ", ")
+		},
+		"image": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			mimes := []string{"image/jpeg", "image/png", "image/gif", "image/bmp", "image/svg+xml", "image/webp"}
+			encoding.ContentType = strings.Join(mimes, ", ")
+		},
+		"count": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			if r.Params[0] != "1" {
+				s.Type = "array"
+				s.Items = &openapi3.SchemaRef{
+					Value: &openapi3.Schema{
+						Type:   "string",
+						Format: "binary",
+					},
+				}
+				count, _ := strconv.ParseUint(r.Params[0], 10, 64)
+				s.MinItems = count
+				s.MaxItems = &count
+			}
+		},
+		"count_min": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Type = "array"
+			s.Items = &openapi3.SchemaRef{
+				Value: &openapi3.Schema{
+					Type:   "string",
+					Format: "binary",
+				},
+			}
+			count, _ := strconv.ParseUint(r.Params[0], 10, 64)
+			s.MinItems = count
+		},
+		"count_max": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Type = "array"
+			s.Items = &openapi3.SchemaRef{
+				Value: &openapi3.Schema{
+					Type:   "string",
+					Format: "binary",
+				},
+			}
+			count, _ := strconv.ParseUint(r.Params[0], 10, 64)
+			s.MaxItems = &count
+		},
+		"count_between": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			s.Type = "array"
+			s.Items = &openapi3.SchemaRef{
+				Value: &openapi3.Schema{
+					Type:   "string",
+					Format: "binary",
+				},
+			}
+			min, _ := strconv.ParseUint(r.Params[0], 10, 64)
+			max, _ := strconv.ParseUint(r.Params[1], 10, 64)
+			s.MinItems = min
+			s.MaxItems = &max
+		},
+		"date": func(r *validation.Rule, s *openapi3.Schema, encoding *openapi3.Encoding) {
+			if len(r.Params) == 0 {
+				if r.Params[0] == time.RFC3339 {
+					s.Format = "date-time"
+				}
+			} else {
+				s.Format = "date"
+			}
+		},
 	}
-	// TODO string formats, arrays, uniqueItems (distinct)
-	// TODO better architecture
 )
